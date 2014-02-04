f3e0485d19083eadc76f4c5c8de57c3df6fde0f5
Matthew Chan <matt@themattchan.com>
2014-02-03 20:57:56 -0800

    soln for #8

---
+++ euler8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from operator import mul
+
+greatest_p = 0
+big_number = open("euler8.txt")
+numlist = [int(num) for num in big_number.read() if num!='\n']
+
+for i in range(5, len(numlist)):
+    product = reduce(mul, numlist[i-5:i])
+    if product > greatest_p:
+        greatest_p = product
+
+print greatest_p
+                     
+                
