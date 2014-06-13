fb40441a2b2d684c8b54ff6d27bb129ca56e5af4
Matthew Chan <matt@themattchan.com>
2014-06-13 02:54:01 -0700

    some haskell

---
+++ euler04.hs
@@ -0,0 +1,14 @@
+import Data.List (sort)
+
+three_digits = [100..999]
+three_products = [x * y | x <- three_digits, y <- three_digits]
+
+isPalindrome :: Int -> Bool
+isPalindrome number = let digits = toList number in
+               digits == reverse (digits)
+             where
+             toList :: Int -> [Int]
+             toList 0 = []
+             toList n =  toList (n `div` 10) ++ [n `mod` 10 ]
+
+main = print (head (reverse (sort (filter isPalindrome three_products))))
--- euler26.c
+++
@@ -1,9 +0,0 @@
-#import <stdio.h>
-
-void main(){
-	long double x = 0;
-	long double largest = 0;
-	for (i=0; i<1000; i++){
-		x=1/i;
-		if (x )
-}
