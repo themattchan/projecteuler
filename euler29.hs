8c4675194d39f9d8c7b91f5509104c4c550e96a4
Matthew Chan <matt@themattchan.com>
2014-06-13 04:22:00 -0700

    fixed indenting for #29

--- euler29.hs
+++ euler29.hs
@@ -3,7 +3,7 @@ bs = [2..100]
 terms = [a^b | a <- as, b <- bs]
 
 len = (length (foldl (\acc x -> if x `elem` acc
-							   then acc
-							   else x : acc) [] terms))
+                               then acc
+                               else x : acc) [] terms))
 
 main = print len
