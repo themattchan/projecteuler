852c3a98c082d82d794435f29b683793ebbae0c4
Matthew Chan <matt@themattchan.com>
2014-06-13 18:25:57 -0700

    #12

--- euler08.py
+++ euler08.py
@@ -3,7 +3,7 @@
 from operator import mul
 
 big_number = open("euler08.txt")
-numlist = [int(num) for num in big_number.read() if num!='\n']
+numlist = [int(num) for num in big_number.read() if num != '\n']
 products = [reduce(mul, numlist[i-13:i]) for i in range(13, len(numlist))]
 
 print sorted(products,reverse=True)[0]
--- euler12.hs
+++ euler12.hs
@@ -1,4 +1,13 @@
 import Data.Numbers.Primes (primeFactors)
-import Data.List (sort)
+import Data.List
 
-triangles = map (\x -> sum [1..x] ) [1..]
+triangles = map (\x -> (x * (x+1) `div` 2)) [1..]
+{-
+from http://mathschallenge.net/library/number/number_of_divisors
+"We begin by writing the number as a product of prime factors: n = paqbrc...
+then the number of divisors, d(n) = (a+1)(b+1)(c+1)..."
+-}
+--dunno what group does, but it won't compile unless its there
+divisors n = product (map ((+1) . length) (group (primeFactors n)))
+res = head $ filter ((> 500) . divisors) triangles
+main = print res
