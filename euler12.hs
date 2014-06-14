8714ba17f829de2e3d9d49f587e4615da4424fd4
Matthew Chan <matt@themattchan.com>
2014-06-13 21:41:13 -0700

    sklfsklf

--- euler12.hs
+++ euler12.hs
@@ -7,7 +7,6 @@ from http://mathschallenge.net/library/number/number_of_divisors
 "We begin by writing the number as a product of prime factors: n = p^a q^b r^c...
 then the number of divisors, d(n) = (a+1)(b+1)(c+1)..."
 -}
---dunno what group does, but it won't compile unless it's there
-divisors n = product (map ((+1) . length) (group (primeFactors n)))
+divisors n = product $ map ((+1) . length) (group (primeFactors n))
 res = head $ filter ((> 500) . divisors) triangles
 main = print res
