151caa31e42fcbf4c7ca9e229d57de5a57823453
Matthew Chan <matt@themattchan.com>
2014-06-13 18:51:10 -0700

    edited readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ projecteuler
 ===========
 
 My attempts to solve Project Euler problems, in whatever language I
-please. I try to make my solutions elegant as possible, which means
+please. I try to make my solutions as elegant as possible, which means
 starting off by using elegant languages like Haskell and Scheme.
 
 Jun 13 2014: I'm rewriting the Python solutions. Too imperative. Too
--- euler12.hs
+++ euler12.hs
@@ -4,10 +4,10 @@ import Data.List
 triangles = map (\x -> (x * (x+1) `div` 2)) [1..]
 {-
 from http://mathschallenge.net/library/number/number_of_divisors
-"We begin by writing the number as a product of prime factors: n = paqbrc...
+"We begin by writing the number as a product of prime factors: n = p^a q^b r^c...
 then the number of divisors, d(n) = (a+1)(b+1)(c+1)..."
 -}
---dunno what group does, but it won't compile unless its there
+--dunno what group does, but it won't compile unless it's there
 divisors n = product (map ((+1) . length) (group (primeFactors n)))
 res = head $ filter ((> 500) . divisors) triangles
 main = print res
