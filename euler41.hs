18ae25b9cc4a14e5d29aad2bb88a8edd228c7d00
Matthew Chan <matt@themattchan.com>
2014-09-11 02:00:06 -0700

    fix

--- euler41.hs
+++ euler41.hs
@@ -2,5 +2,5 @@ import Data.Char (intToDigit)
 import Data.Numbers.Primes
 import Data.List
 euler41 = maximum [num | n <- [1..9],
-				   num <- permutations ['1'.. intToDigit n],
-				   isPrime $ read num]
+                   num <- permutations ['1'.. intToDigit n],
+                   isPrime $ read num]
--- euler43.hs
+++ euler43.hs
@@ -1,11 +1,12 @@
 import Data.List
-euler43 = sum [num' | num <- permutations ['0' .. '9'],
-			   let num' = read num,
-			   let sub3 start = read $ take 3 $ drop (start-1) num,
-			   mod (sub3 2) 2 == 0,
-			   mod (sub3 3) 3 == 0,
-			   mod (sub3 4) 5 == 0,
-			   mod (sub3 5) 7 == 0,
-			   mod (sub3 6) 11 == 0,
-			   mod (sub3 7) 13 == 0,
-			   mod (sub3 8) 17 == 0]
+euler43 = sum [num' |
+			   num <- permutations ['0' .. '9'],
+               let num' = read num,
+               let sub3 start = read $ take 3 $ drop (start-1) num,
+               mod (sub3 2) 2 == 0,
+               mod (sub3 3) 3 == 0,
+               mod (sub3 4) 5 == 0,
+               mod (sub3 5) 7 == 0,
+               mod (sub3 6) 11 == 0,
+               mod (sub3 7) 13 == 0,
+               mod (sub3 8) 17 == 0]
