3e272e1eae1c3977d7f67f753a9e29a395343fb0
Matthew Chan <matt@themattchan.com>
2015-03-30 23:49:22 -0700

    48

--- euler48.hs
+++ euler48.hs
@@ -1,1 +1,1 @@
-euler48 = reverse $ take 10 $ reverse $ show $ sum $ map (\x -> x^x) [1..1000]
+euler48 = reverse . take 10 . reverse . show . sum $ map (\x -> x^x) [1..1000]
--- euler48.py
+++
@@ -1,5 +0,0 @@
-a=[]
-for i in range(1, 1001):
-    a.append(i**i)
-a=str(sum(a))
-print a[len(a)-10:len(a)-1]
