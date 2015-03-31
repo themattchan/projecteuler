16e24e96de1a9a441f27f2f6cd171d82b241b42a
Matthew Chan <matt@themattchan.com>
2015-03-31 00:20:38 -0700

    beautify

--- euler04.hs
+++ euler04.hs
@@ -1,5 +1,5 @@
 import Data.List (sort)
 
 euler04 = last . sort $ filter isPalindrome three_products
-  where three_products =  zipWith (*) [100..999] [100..999]
+  where three_products = [x*y | x<-[100..999], y<-[100..999]]
         isPalindrome n = let s = show n in reverse s == s
