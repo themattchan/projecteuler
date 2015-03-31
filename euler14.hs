4c5a11a6b4fefa14285a06bc8337ccfc602f3e81
Matthew Chan <matt@themattchan.com>
2015-03-31 00:27:21 -0700

    beautify

--- euler14.hs
+++ euler14.hs
@@ -1,8 +1,15 @@
+import Data.List
+
+-- THIS IS REALLY SLOW
 
 collatz :: Int -> Int
 collatz n
   | even n = n `div` 2
-  | odd n = (3*n) + 1
+  | odd  n = 3*n + 1
 
 collatzChain :: Int -> Int
 collatzChain = length . takeWhile (/=1) . iterate collatz
+
+euler14 = fst . head . sortBy cmp . zip mil $ map collatzChain mil
+  where mil = [1..1000000]
+        cmp (_,c1) (_,c2) = compare c1 c2
