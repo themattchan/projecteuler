1111cd8c2fa2ffa707f94d7e2255b5106307093e
Matthew Chan <matt@themattchan.com>
2015-03-30 23:03:09 -0700

    19

--- euler19.hs
+++ euler19.hs
@@ -1,8 +1,9 @@
 import Data.Time.Calendar
 import Data.Time.Calendar.WeekDate
 
-ans =length [() |
-             y <- [1901..2000],
-             m <- [1..12],
-             let (_, _, d) = toWeekDate $ fromGregorian y m 1,
-             d == 7]
+euler19 = length . filter id
+  $ do
+  y  <- [1901..2000]
+  m  <- [1..12]
+  let (_,_,d) = toWeekDate $ fromGregorian y m 1
+  return $ d == 7
