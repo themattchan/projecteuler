08df2efa801f5322a9a225d3c9555766b467e63b
Matthew Chan <matt@themattchan.com>
2015-03-31 00:17:30 -0700

    beautify

--- euler04.hs
+++ euler04.hs
@@ -1,14 +1,5 @@
 import Data.List (sort)
 
-three_digits = [100..999]
-three_products = [x * y | x <- three_digits, y <- three_digits]
-
-isPalindrome :: Int -> Bool
-isPalindrome number = let digits = toList number in
-    digits == reverse (digits)
-  where
-    toList :: Int -> [Int]
-    toList 0 = []
-    toList n =  toList (n `div` 10) ++ [n `mod` 10 ]
-
-euler04 = head $ reverse $ sort $ filter isPalindrome three_products
+euler04 = last . sort $ filter isPalindrome three_products
+  where three_products =  zipWith (*) [100..999] [100..999]
+        isPalindrome n = let s = show n in reverse s == s
