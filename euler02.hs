b3625693b0004ee76714d07f82ed72727decd8da
Matthew Chan <matt@themattchan.com>
2015-03-31 00:13:56 -0700

    beautify

--- euler02.hs
+++ euler02.hs
@@ -1,3 +1,3 @@
 euler02 = sum $ takeWhile (< 4000000) even_fibs
   where fibs      = 0 : 1 : zipWith (+) fibs (tail fibs)
-        even_fibs = filter ((==0) . (`mod` 2)) fibs
+        even_fibs = filter ((== 0) . (`mod` 2)) fibs
