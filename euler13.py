dd80b888fbb82d9fed800959ceb5f242475d9e83
Matthew Chan <matt@themattchan.com>
2015-06-01 00:10:12 -0700

    cleanup

--- euler13.py
+++ euler13.py
@@ -1,4 +1,4 @@
 from operator import add
-num = [ long(x) for x in open("euler13.txt").read().split('\n') if x!='']
-print str(reduce(add, num))[0:10]
 
+num = [long(x) for x in open("euler13.txt").read().split('\n') if x != '']
+print str(reduce(add, num))[0:10]
--- euler17.hs
+++ euler17.hs
@@ -5,15 +5,15 @@ units = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
 numToWord x
-    | x == 0 = []
-    | x < 20 = units !! (x-1)
-    | x >= 20 && x < 100 =
-        tens !! (firstDigit (x) -2) ++ numToWord (x - firstDigit(x) *10)
-    | x < 1000 && x `mod` 100 == 0 =
-        units !! (firstDigit (x)-1) ++ "hundred"
-    | x > 100 && x <= 999 =
-        units !! (firstDigit (x)-1) ++ "hundred" ++ "and" ++ numToWord (x - firstDigit(x) *100)
-    | x == 1000 = "onethousand"
-  where firstDigit x = digitToInt . head . show $ x
+  | x == 0 = []
+  | x < 20 = units !! (x-1)
+  | x >= 20 && x < 100 =
+      tens !! (firstDigit x -2) ++ numToWord (x - (firstDigit x) *10)
+  | x < 1000 && x `mod` 100 == 0 =
+      units !! (firstDigit x -1) ++ "hundred"
+  | x > 100 && x <= 999 =
+      units !! (firstDigit x -1) ++ "hundred" ++ "and" ++ numToWord (x - (firstDigit x) *100)
+  | x == 1000 = "onethousand"
+  where firstDigit = digitToInt . head . show
 
 answer = length . concatMap numToWord $ [1..1000]
