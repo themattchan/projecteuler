73d32d9ec021c47fffa74ea3a985d8b047d4ce6c
Matthew Chan <matt@themattchan.com>
2015-05-31 23:59:45 -0700

    clean 06

--- euler06.rkt
+++ euler06.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-(define 0->100 (for/list ([i 101]) i))
+(define 0-100 (for/list ([i 101]) i))
 (define sum (curry apply +))
 (define (square x) (* x x))
 
 
-(- (square (sum 0->100))
-   (sum (map square 0->100)))
+(- (square (sum 0-100))
+   (sum (map square 0-100)))
