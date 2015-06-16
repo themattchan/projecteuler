39be39d0f4e440d84bed7bda875d4eae739d2626
Matthew Chan <matt@themattchan.com>
2015-06-16 02:21:01 -0700

    fix comments

--- euler11.py
+++ euler11.py
@@ -9,28 +9,28 @@ for line in grid:
     row = map(int, row)
     numarray.append(row)
 
-#horizontal
+# horizontal
 for i in range(len(numarray)):
     for j in range(4, len(numarray[i])):
         product = reduce(mul, numarray[i][j-4:j])
         if product > greatest_p:
             greatest_p = product
 
-#vertical
+# vertical
 for j in range(len(numarray[0])):
     for i in range(len(numarray)-3):
         product = numarray[i][j]*numarray[i+1][j]*numarray[i+2][j]*numarray[i+3][j]
         if product > greatest_p:
             greatest_p = product
-        
-#horizontal L-R
+
+# diagonal L-R
 for i in range(len(numarray[0])-3):
     for j in range(len(numarray)-3):
         product = numarray[i][j]*numarray[i+1][j+1]*numarray[i+2][j+2]*numarray[i+3][j+3]
         if product > greatest_p:
             greatest_p = product
 
-#horizontal R-L
+# diagonal R-L
 for i in range(3, len(numarray)):
     for j in range(len(numarray[0])-3):
         product = numarray[i][j]*numarray[i-1][j+1]*numarray[i-2][j+2]*numarray[i-3][j+3]
