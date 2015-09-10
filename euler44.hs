3e26a1007d335266a31f72ef367d64dfdd9fa57f
Matthew Chan <matt@themattchan.com>
2015-09-10 11:46:11 -0700

    euler44

--- euler44.hs
+++ euler44.hs
@@ -1,13 +1,13 @@
 import Control.Monad
 
 euler44 =
-  let pairs = [(a,b) | a <- [1..], b <- [1..(a-1)]]
-      pent n = (n * (3*n - 1)) `div` 2
+  let pent n = (n * (3*n - 1)) `div` 2
       isPent x = let x' = fromIntegral x
                      n  = (sqrt(24*x' + 1) + 1) / 6 in
         n == fromInteger (round n)
   in head $ do
-      (a,b) <- pairs
+      a <- [1..]
+      b <- [1..(a-1)]
       let pa = pent a
           pb = pent b
       guard $ isPent (pa - pb) && isPent (pb + pa)
