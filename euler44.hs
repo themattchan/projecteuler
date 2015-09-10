3dffb309d503fadbb81e62c643de23d38f5ebd2b
Matthew Chan <matt@themattchan.com>
2015-09-10 11:48:23 -0700

    euler44

--- euler44.hs
+++ euler44.hs
@@ -1,10 +1,10 @@
 import Control.Monad (guard)
 
 euler44 =
-  let pent n = (n * (3*n - 1)) `div` 2
+  let pent   n = (n * (3*n - 1)) `div` 2
       isPent x = let x' = fromIntegral x
-                     n  = (sqrt(24*x' + 1) + 1) / 6 in
-        n == fromInteger (round n)
+                     n  = (sqrt(24*x' + 1) + 1) / 6
+        in n == fromInteger (round n)
   in head $ do
       a <- [1..]
       b <- [1..(a-1)]
